309ec35ce42db6083e30c82f3f6a62d35896c9e4
Shafeen Rahman <shafeenr@tuta.io>
2017-09-30 10:02:02 -0400

    Added more functionality

--- ARF/ARF.ino
+++ ARF/ARF.ino
@@ -1,5 +1,6 @@
-#include "SPI.h"
-#include "Phpoc.h"
+#include <SPI.h>
+#include <Phpoc.h>
+#include <Servo.h>
 
 #define clientGreetInterval 3600000
 
@@ -9,19 +10,42 @@ boolean clientConnection = false;
 
 String userTimeInput;
 
-unsigned long startMillis = 0;
+unsigned long clientConnectionTimer = 0;
+
+Servo dryFood;
+Servo wetFood;
+
+PhpocEmail feedAlert;
+
+PhpocDateTime realTimeClock;
+
+String currentTime;
+
+int schedule[14];
+int spaceLocationX;
+int spaceLocationY;
 
 void setup() {
-  startMillis = millis();
+  dryFood.attach(9);
+  wetFood.attach(10);
+
+  clientConnectionTimer = millis();
 
   Serial.begin(9600);
 
-  Phpoc.begin(PF_LOG_SPI | PF_LOG_NET);
+  Phpoc.begin(PF_LOG_SPI | PF_LOG_NET | PF_LOG_APP);
 
   sshServer.beginSSH("root", "toor");
 
   Serial.print("SSH server address: ");
   Serial.println(Phpoc.localIP());
+
+  feedAlert.setOutgoingServer("smtp.gmail.com", 587);
+  feedAlert.setOutgoingLogin("your_login_id", "your_login_password or app_password");
+
+  feedAlert.setFrom("from_email_address", "from_user_name");
+  feedAlert.setTo("to_email_address", "to_user_name");
+  feedAlert.setSubject("Mail from your Automated Robotic Feeder");
 }
 
 void loop() {
@@ -40,9 +64,56 @@ void loop() {
       sshServer.print(thisChar);
       userTimeInput.concat(thisChar);
     }
+    if (userTimeInput.endsWith("clear")) {
+      userTimeInput = "";
+    }
+    if (userTimeInput.endsWith("feed")) {
+      sshClient.println("Attempting to feed dog.");
+      userTimeInput = "";
+      feedPet();
+    }
+    if (userTimeInput.endsWith("time")) {
+      sshClient.println(realTimeClock.date("Y-m-d H:i:s"));
+    }
+    if (userTimeInput.endsWith("OK")) {
+      spaceLocationX = userTimeInput.indexOf(" ");
+      spaceLocationY = userTimeInput.indexOf(" ", spaceLocationX);
+      schedule[0] = userTimeInput.substring(0, spaceLocationX).toInt();
+      for (int i = 1; i < sizeof(schedule); i++) {
+        schedule[i] = userTimeInput.substring(spaceLocationX + 1, spaceLocationY).toInt();
+        spaceLocationX = userTimeInput.indexOf(" ", spaceLocationY);
+        spaceLocationY = userTimeInput.indexOf(" ", spaceLocationX);
+      }
+      userTimeInput = "";
+    }
   }
-  if (millis() > startMillis + clientGreetInterval) {
-    startMillis = millis();
+  if (millis() > clientConnectionTimer + clientGreetInterval) {
+    clientConnectionTimer = millis();
     clientConnection = false;
   }
 }
+
+void feedPet() {
+  currentTime = realTimeClock.date("Y-m-d H:i:s");
+
+  for (int i = 0; i <= 180; i++) {
+    dryFood.write(i);
+    delay(15);
+  }
+  for (int i = 180; i >= 0; i--) {
+    dryFood.write(i);
+    delay(15);
+  }
+  emailAlert("The Automated Robotic Feeder attempted to feed your pet.", currentTime);
+}
+
+void emailAlert(String message, String recordedTime) {
+  feedAlert.beginMessage();
+  feedAlert.println("Hello, user!");
+  feedAlert.println(message);
+  feedAlert.println("This action was performed at ");
+  feedAlert.print(recordedTime);
+  feedAlert.endMessage();
+  feedAlert.send();
+}
+
